c773a1b17be4ef979455a1075255b36e13c3175f
Hannah Neal <hannahmneal@gmail.com>
2019-03-21 14:21:08 -0500

    adding code from class and in chapter for practice setup.

--- StudentExercise4/Program.cs
+++ StudentExercise4/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace StudentExercise4
 {
@@ -6,7 +8,17 @@ namespace StudentExercise4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //(^_^) INSTRUCTIONS:
+
+            /*
+                    Write the necessary C# code in Repository.cs and Program.cs to perform the following actions. Make sure to print results of each action to the console.
+                         1. Query the database for all the Exercises.
+                        2. Find all the exercises in the database where the language is JavaScript.
+                        3. Insert a new exercise into the database.
+                        4. Find all instructors in the database. Include each instructor's cohort.
+                        5. Insert a new instructor into the database.Assign the instructor to an existing cohort.
+                        6. Assign an existing exercise to an existing student.
+             * /*/
         }
     }
 }
---
+++ StudentExercise4/Repository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentExercise4
+{
+    class Repository
+    {
+
+        //(^_^) INSTRUCTIONS:
+
+        /*
+             Write the necessary C# code in Repository.cs and Program.cs to perform the following actions. Make sure to print results of each action to the console.
+                     1. Query the database for all the Exercises.
+                    2. Find all the exercises in the database where the language is JavaScript.
+                    3. Insert a new exercise into the database.
+                    4. Find all instructors in the database. Include each instructor's cohort.
+                    5. Insert a new instructor into the database.Assign the instructor to an existing cohort.
+                    6. Assign an existing exercise to an existing student.
+         * /*/
+
+
+    }
+}
